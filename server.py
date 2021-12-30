538dbd25861f223d285225d0950bbc1b76129485
YassineALL <96870479+YassineALL@users.noreply.github.com>
2021-12-30 14:38:06 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_symptom_names', methods=['GET'])
+def get_symptom_names():
+    response = jsonify({
+        'symptom': util.get_data_columns()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/get_diseases_names', methods=['GET'])
+def get_diseases_names():
+    response = jsonify({
+        'diseases': util.get_diseases_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_disease', methods = ['GET','POST'])
+def predict_disease():
+
+
+    S1 = request.form['S1']
+    S2 = request.form['S2']
+    S3 = request.form['S3']
+    S4 = request.form['S4']
+    S5 = request.form['S5']
+    S6 = request.form['S6']
+
+    response = jsonify({
+        'predict_disease': util.predict_disease(S1,S2,S3,S4,S5,S6)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/description', methods = ['GET'])
+def description():
+    response = jsonify({
+        'Description of the disease': util.description()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/precaution', methods = ['GET'])
+def precaution():
+    response = jsonify({
+        'Precaution of the disease': util.precaution()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    util.load_saved_artifacts()
+    app.run()
---
+++ util.py
@@ -0,0 +1,79 @@
+import pickle
+import json
+import numpy as np
+import pandas as pd
+
+__diseases = None
+__data_columns = None
+__model = None
+
+df1 = pd.read_csv("D:/actimi/deployment/Predict_Disease/server/Symptom-severity.csv")
+desc = pd.read_excel("D:/actimi/deployment/Predict_Disease/server/symptom_Description.xlsx")
+prec = pd.read_excel("D:/actimi/deployment/Predict_Disease/server/symptom_precaution.xlsx")
+
+def predict_disease(S1,S2,S3,S4,S5,S6):
+
+
+    psymptoms = [S1, S2, S3, S4, S5, S6]
+
+
+    #print(psymptoms)
+    a = np.array(df1["Symptom"])
+    b = np.array(df1["weight"])
+    for j in range(len(psymptoms)):
+        for k in range(len(a)):
+            if psymptoms[j] == a[k]:
+                psymptoms[j] = b[k]
+
+    psy = [psymptoms]
+
+    with open('./artifacts/SympDetector.pkl', 'rb') as f:
+        __model = pickle.load(f)
+
+    pred2 = __model.predict(psy)
+    #print(pred2[0])
+    global __a
+    __a = desc.index[desc['diseases'] == str(pred2[0])][0]
+
+    return (pred2[0])
+
+
+def description():
+
+   return(desc.loc[__a, 'description'])
+
+
+def precaution():
+
+    return (prec.loc[__a, 'Precaution_1'],prec.loc[__a, 'Precaution_2'],prec.loc[__a, 'Precaution_3'],prec.loc[__a, 'Precaution_4'])
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __diseases
+
+    with open("./artifacts/columns_symptom.json", "r") as f:
+        __data_columns = json.load(f)
+
+    with open("./artifacts/columns_diseases.json", "r") as ff:
+        __diseases = json.load(ff)['data_columns']
+
+    global __model
+    if __model is None:
+        with open('./artifacts/SympDetector.pkl', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+def get_diseases_names():
+    return __diseases
+
+def get_data_columns():
+    return __data_columns
+
+
+if __name__ == '__main__':
+
+
+    predict_disease('chills', 'skin_rash', 'shivering', 'weight_gain', 'muscle_wasting', 'loss_of_appetite')
+
+
